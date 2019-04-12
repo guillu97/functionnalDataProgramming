4e1f915feb1a5a7a6a97ac3f22d94eed405abcc8
ClaireH97 <claire.huang@efrei.net>
2019-04-13 01:45:39 +0200

    implemented 6 functionalities - get average time used - get in area (tocheck) - get average km done since last charge - get maximun speed all scooter - get average spped scooter in use - get nb scooter battery lower than 20%

--- src/main/scala/com/funct/project/spark/SparkFunct.scala
+++ src/main/scala/com/funct/project/spark/SparkFunct.scala
@@ -8,12 +8,15 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object SparkFunct extends App {
   /*case class SparkFunct(){
     println("testTest")
   }*/
+
+  var totalAvgTimeUse = 0
 
   def jsonStrToMap(jsonStr: String): Map[String, Any] = {
     implicit val formats = org.json4s.DefaultFormats
@@ -21,9 +24,14 @@ object SparkFunct extends App {
     parse(jsonStr).extract[Map[String, Any]]
   }
 
+  def avgTimeUse(line : String) = {
+    val linemap = jsonStrToMap(line)
+    totalAvgTimeUse = totalAvgTimeUse + linemap("timeInUse").toString.toInt
+  }
+
   val conf = new SparkConf().setAppName("DirectStream").setMaster("local[*]")
 
-  val ssc = new StreamingContext(conf, Seconds(20))
+  val ssc = new StreamingContext(conf, Seconds(5))
 
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "localhost:9092",
@@ -41,40 +49,46 @@ object SparkFunct extends App {
     Subscribe[String, String](topics, kafkaParams)
   )
 
-  stream.map(record => record.value)
-    .foreachRDD(rdd => {
-      val d = rdd.collect().foreach(line => {
-        println(line)
-      })
-    })
+  val spark = SparkSession.builder().appName("Spark SQL").getOrCreate()
+
+  stream.foreachRDD(rddRaw => {
+    val rdd = rddRaw.map(_.value.toString)
+    val df = spark.read.json(rdd)
+    if(!df.isEmpty) {
+
+      val avgTimeUsed = df.select(avg("timeInUse"))
+      println("\n"+"Nb = " + avgTimeUsed.show() + "\n")
+
+      val lowBattery =
+          df
+          .filter("battery < 20")
+          .select("id")
+          .distinct()
+          .count()
+      println("\n" + "Number of device with low battery : " + lowBattery + "\n")
+
+      val avgKmLastCharge =
+        df.select(avg("km.afterLastCharge"))
+      println("\n"+"km last charge = " + avgKmLastCharge.show() + "\n")
+
+      val avgSpeedInUse =
+        df.filter("inUse == 'true'").select(avg("speed"))
+      println("\n"+"avg speed scooter in use = " + avgSpeedInUse.show() + "\n")
+
+      val maxSpeedAll =
+        df.select(max("speed"))
+      println("\n max speed = " + maxSpeedAll.show())
+
+      /*val positionLat = 2
+      val positionLong = 50
+      val inArea = df.filter("(coord.lat > positionLat- 500 AND coord.lat < positionLat + 500) AND (coord.long > positionLong- 500 AND coord.long < positionLong + 500)")
+          .select("id")
+          .distinct()
+          .count()
+      println("\n" + "Number of device with in area : " + inArea + "\n")*/
+
+    }
+  })
   ssc.start()
   ssc.awaitTermination()
 }
-
-/*
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-
-class SparkFunct{
-
-  val kafkaParams = Map[String, Object](
-    "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
-    "key.deserializer" -> classOf[StringDeserializer],
-    "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "use_a_separate_group_id_for_each_stream",
-    "auto.offset.reset" -> "latest",
-    "enable.auto.commit" -> (false: java.lang.Boolean)
-  )
-
-  val topics = Array("test-records")
-  val stream = KafkaUtils.createDirectStream[String, String](
-    streamingContext,
-    PreferConsistent,
-    Subscribe[String, String](topics, kafkaParams)
-  )
-
-  stream.map(record => (record.key, record.value))
-}*/
